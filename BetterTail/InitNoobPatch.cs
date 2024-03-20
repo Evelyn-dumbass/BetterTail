78820a2dd20bf139b1d51d95a25dedfd33932163
Evelyn <132259285+Evelyn-dumbass@users.noreply.github.com>
2024-03-20 14:20:44 +0000

    tail

---
+++ BetterTail/InitNoobPatch.cs
@@ -0,0 +1,17 @@
+﻿using GorillaExtensions;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BetterTail
+{
+    [HarmonyPatch(typeof(VRRig), "InitializeNoobMaterial")]
+    internal class InitNoobPatch
+    {
+        public static void Postfix(ref VRRig __instance)
+        {
+            Transform Tail = __instance.transform.Find("rig/body/2024_03_Nowruz Body/LBAFW./TigerTail");
+            Tail.GetComponent<SkinnedMeshRenderer>().material = Main.furMat;
+            Tail.GetComponent<SkinnedMeshRenderer>().material.color = __instance.playerColor;
+        }
+    }
+}
---
+++ BetterTail/Main.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace BetterTail
+{
+    [BepInPlugin("Evelyn.BetterTail", "BetterTail", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public static Material furMat;
+        void Awake() { GorillaTagger.OnPlayerSpawned(OnGameInit); new Harmony("Eve.BetterTail").PatchAll(Assembly.GetExecutingAssembly()); }
+
+        async void OnGameInit()
+        {
+            furMat = Instantiate(LoadAssetBundle("BetterTail.Resource.fur").LoadAsset<Material>("furMat"));
+
+            await Task.Delay(3000);
+
+            GameObject Tail = GorillaTagger.Instance.offlineVRRig.transform.Find("rig/body/2024_03_Nowruz Body/LBAFW./TigerTail").gameObject;
+            Tail.GetComponent<SkinnedMeshRenderer>().material = furMat;
+            Tail.GetComponent<SkinnedMeshRenderer>().material.color = GorillaTagger.Instance.offlineVRRig.mainSkin.material.color;
+        }
+
+        public AssetBundle LoadAssetBundle(string path)
+        {
+            Assembly.GetExecutingAssembly().GetManifestResourceStream(path).Close();
+            return AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream(path));
+        }
+    }
+}
